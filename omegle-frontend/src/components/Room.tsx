b9672d63859d45f54ff15a0c347852f6aa4b93e1
nst-student <khuswantrajpurohit18@gmail.com>
2026-02-15 04:53:17 +0530

    feat: implement routing with Landing and Room components, update TypeScript configuration, and add react-router-dom dependency

---
+++ omegle-frontend/src/components/Room.tsx
@@ -0,0 +1,17 @@
+import React, { useEffect } from 'react'
+import { useSearchParams } from 'react-router-dom'
+
+const Room = () => {
+    const [searchParams,setSearchParams] = useSearchParams() 
+    const name = searchParams.get('name') 
+    useEffect(()=>{
+
+    },[name])
+  return (
+    <div>
+      Hi {name} 
+    </div>
+  )
+}
+
+export default Room
