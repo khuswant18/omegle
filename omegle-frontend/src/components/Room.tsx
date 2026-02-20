7358af8f79835740e0aed5aa39c6210177d6fcb7
nst-student <khuswantrajpurohit18@gmail.com>
2026-02-20 14:00:59 +0530

    feat: configure environment variable for API URL and update Room component

--- omegle-frontend/src/components/Room.tsx
+++ omegle-frontend/src/components/Room.tsx
@@ -2,7 +2,8 @@ import { useEffect, useState, useRef } from "react";
 // import { useSearchParams } from "react-router-dom";
 import { io } from "socket.io-client";
 
-const URL = "http://localhost:3000";
+// const URL = "http://localhost:3000";
+const URL = import.meta.env.VITE_API_URL
 
 const Room = ({
   name,
